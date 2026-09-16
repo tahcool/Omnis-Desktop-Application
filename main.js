f0bfc8ad3073f56c13a2bd167a16a7569320ffdd
tahcool <tahcul@gmail.com>
2026-09-16 14:23:30 +0200

    Fix: support params.filters in update handler for chainable .eq() from preload proxy - fixes WHERE clause error

--- main.js
+++ main.js
@@ -878,6 +878,12 @@ ipcMain.handle('supabase:query', async (event, { table, method, params, data }) 
       if (params.name) query = query.eq('name', params.name);
       else if (params.id) query = query.eq('id', params.id);
       else if (params.match) query = query.match(params.match);
+      // Support chainable .eq() filters from preload proxy
+      if (params.filters && typeof params.filters === 'object') {
+        for (const [col, val] of Object.entries(params.filters)) {
+          if (val !== undefined && val !== null) query = query.eq(col, val);
+        }
+      }
       if (!params.skipSelect) query = query.select();
 
     } else if (method === 'upsert') {
